80d13b96c66fd68346b25ee76e14bc7f9963ff58
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-06-18 14:35:34 +1200

    established connection to localhost

--- app.py
+++ app.py
@@ -2,4 +2,14 @@ import pymysql
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+def create_connection():
+    return pymysql.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="Abcdefg123!",
+        # DATABASE
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
 app.run(debug=True)
